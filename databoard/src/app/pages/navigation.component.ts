a985619ca9f89a8c363790aa3f02bdaca8b26864
microcosmx <15110240026@fudan.edu.cn>
2018-05-07 18:38:37 +0800

    ref

---
+++ databoard/src/app/pages/navigation.component.ts
@@ -0,0 +1,10 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dt-navigation',
+  templateUrl: './navigation.component.html'
+})
+export class NavigationComponent {
+
+}
---
+++ databoard/src/app/pages/pages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { MENU_ITEMS } from './pages-menu';
+
+@Component({
+  selector: 'ngx-pages',
+  templateUrl: './pages.component.html'
+})
+export class PagesComponent {
+
+  menu = MENU_ITEMS;
+}
