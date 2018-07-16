0105cd593099c4f64ff5b9c331cb955496250038
microcosmx <15110240026@fudan.edu.cn>
2018-07-16 20:08:12 +0800

    ref

--- spca/src/app/pages/charts/d3/x-axis-ticks-ext.component.ts
+++ spca/src/app/pages/charts/d3/x-axis-ticks-ext.component.ts
@@ -28,10 +28,10 @@ import {
           [attr.transform]="textTransform"
           [style.font-size]="'12px'">
           <tspan x="0" y="0">{{xtickInfos[tick] && xtickInfos[tick].reason || ""}}</tspan>
-          <tspan x="12" y="24">{{xtickInfos[tick] && xtickInfos[tick].runid || ""}}</tspan>
-          <tspan x="0" y="48">{{xtickInfos[tick] && xtickInfos[tick].date || ""}}</tspan>
+          <tspan x="12" y="16">{{xtickInfos[tick] && xtickInfos[tick].runid || ""}}</tspan>
+          <tspan x="0" y="32">{{xtickInfos[tick] && xtickInfos[tick].date || ""}}</tspan>
         </text>
-        <svg:image *ngIf="xtickInfos[tick] && xtickInfos[tick].runstate==='pass'" x="-30" y="8" width="24" height="24" xlink:href="assets/images/pass.png"></image>
+        <svg:image *ngIf="xtickInfos[tick] && xtickInfos[tick].runstate==='pass'" x="-20" y="4" width="16" height="16" xlink:href="assets/images/pass.png"></image>
       </svg:g>
     </svg:g>
 
